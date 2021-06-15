cc45074380b68b633a122874a8c07ed9a0fe8eb6
Tom Tang <chengchi.tang.work@gmail.com>
2021-06-15 23:16:45 +0800

    Add some error string

--- DateAndTime.py
+++ DateAndTime.py
@@ -6,7 +6,7 @@ class DateAndTime:
         self.hour = hour_in_24
         self.minute = minute
     def __str__(self):
-        if int(self.hour) > 24 or self.hour < 0:
+        if int(self.hour) > 24 or int(self.hour) < 0:
             return 'error'
         return self.year +'-' + self.month + '-' + self.day + ' ' + self.hour + ':' + self.minute
 
@@ -14,5 +14,5 @@ class DateAndTime:
 dateAndTime1 = DateAndTime('2019', '11', '30', '22', '31')
 print(dateAndTime1) # 2019-11-30 22:31
 
-dateAndTime2 = DateAndTime('2020', '11', '30', '23', '00')
+dateAndTime2 = DateAndTime('2020', '11', '30', '25', '00')
 print(dateAndTime2) # 2019-11-30 22:31
