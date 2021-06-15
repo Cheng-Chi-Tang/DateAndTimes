b0174110d3d764ee5b29e9eb43a2fcb5c27b27b9
Tom Tang <chengchi.tang.work@gmail.com>
2021-06-15 23:15:40 +0800

    /Library/Frameworks/Python.framework/Versions/3.8/bin/python3.8 /Users/Shiauji/DifferenceBetweenDateAndTimes/DateAndTime.py

--- DateAndTime.py
+++ DateAndTime.py
@@ -6,9 +6,13 @@ class DateAndTime:
         self.hour = hour_in_24
         self.minute = minute
     def __str__(self):
+        if int(self.hour) > 24 or self.hour < 0:
+            return 'error'
         return self.year +'-' + self.month + '-' + self.day + ' ' + self.hour + ':' + self.minute
 
 # This is demo
-dateAndTime = DateAndTime('2019', '11', '30', '22', '31')
-print(dateAndTime)
-    
+dateAndTime1 = DateAndTime('2019', '11', '30', '22', '31')
+print(dateAndTime1) # 2019-11-30 22:31
+
+dateAndTime2 = DateAndTime('2020', '11', '30', '23', '00')
+print(dateAndTime2) # 2019-11-30 22:31
