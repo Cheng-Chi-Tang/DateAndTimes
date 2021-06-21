8fda9f3934cf5cacda19f3efec35d0736f1c152b
Tom Tang <chengchi.tang.work@gmail.com>
2021-06-21 21:27:58 +0800

    Add error conditions for common year and leap year

--- DateAndTime.py
+++ DateAndTime.py
@@ -1,4 +1,6 @@
 class DateAndTime:
+    CONST_LEAP_YEAR_DAYS = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    CONST_COMM_YEAR_DAYS = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     def __init__(self, year, month, day, hour_in_24, minute):
         self.year = year
         self.month = month
@@ -6,8 +8,12 @@ class DateAndTime:
         self.hour = hour_in_24
         self.minute = minute
     def __str__(self):
-        if self.hour > 24 or self.hour < 0:
-            return 'error'
+        if self.year % 4 == 0 and self.year % 100 == 0 and self.year % 400 == 0: # Leap Year Condition
+            if self.hour > 24 or self.hour < 0 or self.month <= 0 or self.month > 12 or self.day <= 0 or self.day > DateAndTime.CONST_LEAP_YEAR_DAYS[self.month - 1]:
+                return 'error'
+        else: # Common Year Condition
+            if self.hour > 24 or self.hour < 0 or self.month <= 0 or self.month > 12 or self.day <= 0 or self.day > DateAndTime.CONST_COMM_YEAR_DAYS[self.month - 1]:
+                return 'error'
         return str(self.year) +'-' + str(self.month) + '-' + str(self.day) + ' ' + str(self.hour) + ':' + str(self.minute)
 
 # This is demo
@@ -16,3 +22,7 @@ print(dateAndTime1) # 2019-11-30 22:31
 
 dateAndTime2 = DateAndTime(2019, 11, 30, 25, 30)
 print(dateAndTime2) # 2019-11-30 22:31
+
+
+dateAndTime3 = DateAndTime(2000, 2, 29, 23, 30)
+print(dateAndTime3) # 2019-11-30 22:31
